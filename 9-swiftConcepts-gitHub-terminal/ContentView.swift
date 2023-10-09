17400680ee519aa0d61f345b44ea1fa6d1792947
ziyiliu02 <ziyi.liu02@gmail.com>
2023-10-09 18:20:06 +0800

    Initial commit

---
+++ 9-swiftConcepts-gitHub-terminal/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  9-swiftConcepts-gitHub-terminal
+//
+//  Created by Liu Ziyi on 9/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 9-swiftConcepts-gitHub-terminal/__swiftConcepts_gitHub_terminalApp.swift
@@ -0,0 +1,17 @@
+//
+//  __swiftConcepts_gitHub_terminalApp.swift
+//  9-swiftConcepts-gitHub-terminal
+//
+//  Created by Liu Ziyi on 9/10/23.
+//
+
+import SwiftUI
+
+@main
+struct __swiftConcepts_gitHub_terminalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
