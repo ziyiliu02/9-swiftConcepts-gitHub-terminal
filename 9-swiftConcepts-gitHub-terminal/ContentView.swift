0c75d217402e43e8caddde3e537225ad8c38cc7e
ziyiliu02 <ziyi.liu02@gmail.com>
2023-10-09 23:13:52 +0800

    Add text

--- 9-swiftConcepts-gitHub-terminal/ContentView.swift
+++ 9-swiftConcepts-gitHub-terminal/ContentView.swift
@@ -11,10 +11,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("GitHub Terminal")
         }
         .padding()
     }
